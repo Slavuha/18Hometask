aa5cfd34e5d26993d83e4bf9978531b3b125ecc9
Mazur <Slavaslidchum@gmail.com>
2022-06-12 10:54:21 +0300

    Bro4

--- src/com/company/Conventer.java
+++ src/com/company/Conventer.java
@@ -1,29 +1,26 @@
 package com.company;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Conventer <T> {
-    public T convertedList;
-    public Conventer(T convertedList) {
-        this.convertedList = convertedList;
-    }
+    private List<T> convertedList;
 
-    public List<T> numericArrayToList(T convertedList) {
-        List<T> arr = new ArrayList();
-        arr.add(convertedList);
-        arr = Arrays.asList(convertedList);
-        return arr;
+    public void setArrayToList(T[] inputArray) {
+        List<T[]> arr = new ArrayList(List.of(inputArray));
+        convertedList = (List<T>) arr;
+       // System.out.println("Print from metod setArratToList " +convertedList);
+
         }
 
-    public T getConvertedList() {
-        return convertedList;
-    }
 
     public void printList() {
-            System.out.println("Print from Class Conventer metod" + Arrays.toString((Object[]) convertedList));
+
+        System.out.println("Print from Class Conventer metod" + convertedList);
         }
 
+    public List<T> getConvertedList() {
+        return convertedList;
+    }
 
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,26 +5,15 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Printer<String> stringPrinterprinter = new Printer<>("Printed test from Main = String");
-        // stringPrinterprinter.print();
 
-        Integer[] arr = {1, 2, 7};
-        Printer<Integer[]> integerPrinter = new Printer<>(arr);
-        integerPrinter.arrToList(arr);
-        integerPrinter.getSomething();
-        //      integerPrinter.print();
-//
-        //       System.out.println("Print iz meina "+integerPrinter);
+        Integer[] arrDimin = {1, 23, 43, 123, -213};
 
-// try from massive to araysList her is working
-        List<Integer> arrayList = new ArrayList();
-        arrayList = Arrays.asList(arr);
-        //      System.out.println("Print from main test 2 "+arrayList);
+        Conventer<Integer> converterInt = new Conventer<>();
+        converterInt.setArrayToList(arrDimin);
 
-        Conventer<Integer[]> conventer = new Conventer<>(arr);
-        conventer.numericArrayToList(arr);
-        conventer.getConvertedList();
-        conventer.printList();
+        List<Integer> intResultList = converterInt.getConvertedList();
+        System.out.println("Print intResult " + intResultList);
+        converterInt.printList();
     }
 }
 
